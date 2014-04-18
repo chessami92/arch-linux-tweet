bb66ee9f670df559fe605387ae6ff2e0214baac4
Josh DeWitt <jdewitt@unomaha.edu>
2014-04-18 13:55:16 -0500

    Empty the pipes before restarting services

--- emergencyStop.c
+++ emergencyStop.c
@@ -6,6 +6,8 @@ int main() {
     system( "echo 'out' > /sys/class/gpio/gpio14/direction" );
     system( "echo '0' > /sys/class/gpio/gpio14/value" );
     system( "sleep 1" );
+    system( "dd if=/home/cnc/gcode iflag=nonblock of=/dev/null" );
+    system( "dd if=/home/cnc/serial-data iflag=nonblock of=/dev/null" );
     system( "echo '1' > /sys/class/gpio/gpio14/value" );
     system( "systemctl restart g-code-interpreter cnc-driver bootload" );
 
