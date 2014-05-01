351ba072fdaba0ba63bb28b6ff6591779c6919c1
Josh DeWitt <jdewitt@unomaha.edu>
2014-05-01 11:32:58 -0500

    Change of order for emergency stop.

--- emergencyStop.c
+++ emergencyStop.c
@@ -5,11 +5,12 @@ int main() {
     system( "echo '14' > /sys/class/gpio/export" );
     system( "echo 'out' > /sys/class/gpio/gpio14/direction" );
     system( "echo '0' > /sys/class/gpio/gpio14/value" );
-    system( "sleep 1" );
+    system( "systemctl stop g-code-interpreter cnc-driver" );
     system( "dd if=/home/cnc/gcode iflag=nonblock of=/dev/null" );
     system( "dd if=/home/cnc/serial-data iflag=nonblock of=/dev/null" );
+    system( "sleep 3" );
     system( "echo '1' > /sys/class/gpio/gpio14/value" );
-    system( "systemctl restart g-code-interpreter cnc-driver bootload" );
+    system( "systemctl start g-code-interpreter cnc-driver" );
 
     return 0;
 }
